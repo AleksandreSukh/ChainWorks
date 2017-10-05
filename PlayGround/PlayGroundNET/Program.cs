f881139db33860e3fea0c3241b6256ac4f69b80b
aleksandresukhitashvili@gmail.com <aleksandresukhitashvili@gmail.com>
2017-10-05 22:26:39 +0400

    Added another project to test stuff in .NET env

---
+++ PlayGround/PlayGroundNET/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PlayGround;
+using Utils;
+
+namespace PlayGroundNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            LinqPadLikeExtensions.Init(s=>MessageBox.Show(s));
+            var stemmer = new Stemmer(@"G:\Source\Repos\TextAnalyser\TextAnalyser\StemmerWrap\database", @"G:\Source\Repos\TextAnalyser\TextAnalyser\StemmerWrap\termTypes.json");
+            stemmer.Lemmatize(new[] { "ცხენმა", "ლომს", "მგლის", "ტყეთა", "წლის" }).Dump();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ PlayGround/PlayGroundNET/Stemmer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
+namespace PlayGround
+{
+    public class Stemmer
+    {
+        internal JsonSerializer Gson;
+        internal TermType[] TermTypes;
+        readonly HashSet<string> _termList = new HashSet<string>();
+
+        public Stemmer(string nounDatabasePath, string termTypesPath)
+        {
+            Gson = new JsonSerializer();
+            foreach (var s in File.ReadAllLines(nounDatabasePath).Distinct().OrderBy(a => a))
+                _termList.Add(s);
+
+            TermTypes = (TermType[])Gson.Deserialize(new JsonTextReader(new StringReader(File.ReadAllText(termTypesPath))), typeof(TermType[]));
+        }
+
+        string LemmatizeTerm(string obj0)
+        {
+            var result = (from termType in TermTypes
+                          from termTypeForm in termType.Forms
+                          let str1 = termTypeForm.TryMatchForm(obj0)
+                          where str1 != null
+                          let str2 = termType.GenerateStem(str1)
+                          where _termList.Contains(str2)
+                          select new { form = termTypeForm, stem = str2 })
+                .OrderBy(a => a.form.Priority).Select(a => a.stem).FirstOrDefault();
+            return result;
+        }
+
+        public string[] Lemmatize(string[] terms) => terms.Select(t => LemmatizeTerm(t)).ToArray();
+
+      
+    }
+    public class TermType
+    {
+        public string Lemma { get; set; }
+        public string Description { get; set; }
+        public TermTypeForm[] Forms { get; set; }
+
+        public string GenerateStem(string partialStem) => Lemma.Replace("*", partialStem);
+    }
+    public class TermTypeForm
+    {
+        public string Form { get; set; }
+        public int Priority { get; set; }
+
+        public string TryMatchForm(string term)
+        {
+            var regex = new Regex(Form.Replace("*", "(.+)"));
+            if (!regex.IsMatch(term)) return null;
+            var partialStem = regex.Match(term).Groups[1].Value;
+            return partialStem;
+        }
+    }
+
+}
+
